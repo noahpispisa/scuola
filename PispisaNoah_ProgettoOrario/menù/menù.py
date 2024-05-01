25874702cb53eb1770459125d5a439c88f9692ea
noahpispisa <167305917+noahpispisa@users.noreply.github.com>
2024-05-01 21:03:39 +0200

    Add files via upload

---
+++ PispisaNoah_ProgettoOrario/menù/menù.py
@@ -0,0 +1,46 @@
+import menù.funzioniutili as f #contiene le funzioni per gli input e le parti di codice usate più spesso
+premessaggio = "Digitare" #prima parte del messaggio
+midmessaggio = "per" #terza parte del messaggio, la seconda è il numero da digitare
+sep = "-"*25 #un separatore
+
+funzioni = ("elenco docenti classe","orario docente","ore disponibili","docenti a lezione","<funzione>","<funzione>","<funzione>","<funzione>","<funzione>","<funzione>","<funzione>")
+def creavoci(tup):
+    """
+    crea la lista con le opzioni possibili
+    parametri:
+    tup: una tupla con le funzioni che verranno usate
+
+    return:
+    ritorna una stringa formattata che contiene le opzioni
+    """
+    voce = ""
+    for i in range(len(tup)):
+        if (tup[i] != "<funzione>"):
+            voce += f"{premessaggio} {i} {midmessaggio} {tup[i]}\n"
+    return voce
+
+def menu():
+    """
+    stampa le opzioni possibili "delimitate" da un separatore sopra e sotto
+    non chiede parametri, non da return
+    """
+    print(sep)
+    print(creavoci(funzioni))
+
+    return 
+
+def scelta(min,max):
+    """
+    stampa il menù e limita la scelta al numero di opzioni disponibili
+    
+    parametri:
+    min: la scelta minima possibili
+    max: la scelta massima possibile
+    
+    return:
+    ritorna un intero che è la scelta effettuata
+    """
+    menu()
+    scelta = f.lim_I("",min,max)
+    return scelta
+
